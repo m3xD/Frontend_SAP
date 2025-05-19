ca957229242cc7ec415f73553f3be6af2998957b
khanh <thaoproduction123@gmail.com>
2025-05-19 17:54:21 +0700

    refactor: update Vite server settings to allow specific host for HMR

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-		allowedHosts: true,
+		allowedHosts: ["sap.m3xd.dev"],
 		hmr: {
-			host: 'localhost',
+			host: 'sap.m3xd.dev',
 			protocol: "ws",
 		}
 	},
