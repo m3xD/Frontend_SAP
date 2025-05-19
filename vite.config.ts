192f218c5d4a950a235e4dbd87343454bf8ea9e7
khanh <thaoproduction123@gmail.com>
2025-05-19 20:06:43 +0700

    refactor: remove specific host configuration for HMR in Vite server settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  	server: {
-		allowedHosts: ["sap.m3xd.dev"],
-		hmr: {
-			host: 'localhost',
-			port: 8080,
-		}
-	}
+	plugins: [react()],
 })
